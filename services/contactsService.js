070e71054ced51f29ca44afd3bf78e603e422099
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-02-17 16:26:33 -0300

    refactor: Improve contactsService.js code

--- services/contactsService.js
+++ services/contactsService.js
@@ -22,38 +22,30 @@ const processContacts = async (domain, hubId, q) => {
   const limit = 100;
 
   while (hasMore) {
-    const lastModifiedDate = offsetObject.lastModifiedDate || lastPulledDate;
-    const lastModifiedDateFilter = generateLastModifiedDateFilter(
-      lastModifiedDate,
+    const searchObject = prepareSearchCriteria(
+      offsetObject,
+      lastPulledDate,
       now,
-      "lastmodifieddate"
+      limit
     );
-    const searchObject = {
-      filterGroups: [lastModifiedDateFilter],
-      sorts: [{ propertyName: "lastmodifieddate", direction: "ASCENDING" }],
-      properties: [
-        "firstname",
-        "lastname",
-        "jobtitle",
-        "email",
-        "hubspotscore",
-        "hs_lead_status",
-        "hs_analytics_source",
-        "hs_latest_source",
-      ],
-      limit,
-      after: offsetObject.after,
-    };
 
+    // Fetch Contacts
     const searchResult = await fetchContacts(searchObject, domain, hubId);
     const data = searchResult.results || [];
 
     console.log("fetch contact batch");
 
+    console.log(searchResult.total);
+
+    if (searchResult.total === 0) {
+      hasMore = false;
+      break;
+    }
+
     offsetObject.after = parseInt(searchResult.paging?.next?.after);
     const contactIds = data.map((contact) => contact.id);
 
-    // contact to company association
+    // Fetch Company Associations
     const companyAssociations = await fetchCompanyAssociations(
       contactIds,
       domain,
@@ -62,34 +54,13 @@ const processContacts = async (domain, hubId, q) => {
 
     data.forEach((contact) => {
       if (!contact.properties || !contact.properties.email) return;
-
-      const companyId = companyAssociations[contact.id];
-      const isCreated = new Date(contact.createdAt) > lastPulledDate;
-
-      const userProperties = {
-        company_id: companyId,
-        contact_name: (
-          (contact.properties.firstname || "") +
-          " " +
-          (contact.properties.lastname || "")
-        ).trim(),
-        contact_title: contact.properties.jobtitle,
-        contact_source: contact.properties.hs_analytics_source,
-        contact_status: contact.properties.hs_lead_status,
-        contact_score: parseInt(contact.properties.hubspotscore) || 0,
-      };
-
-      const actionTemplate = {
-        includeInAnalytics: 0,
-        identity: contact.properties.email,
-        userProperties: filterNullValuesFromObject(userProperties),
-      };
-
-      q.push({
-        actionName: isCreated ? "Contact Created" : "Contact Updated",
-        actionDate: new Date(isCreated ? contact.createdAt : contact.updatedAt),
-        ...actionTemplate,
-      });
+      // processContactAction(companyAssociations, contact, lastPulledDate, q);
+      console.log(
+        "processContactAction",
+        companyAssociations,
+        contact,
+        lastPulledDate
+      );
     });
 
     if (!offsetObject?.after) {
@@ -108,5 +79,61 @@ const processContacts = async (domain, hubId, q) => {
 
   return true;
 };
+
+function prepareSearchCriteria(offsetObject, lastPulledDate, now, limit) {
+  const lastModifiedDate = offsetObject.lastModifiedDate || lastPulledDate;
+  const lastModifiedDateFilter = generateLastModifiedDateFilter(
+    lastModifiedDate,
+    now,
+    "lastmodifieddate"
+  );
+  const searchObject = {
+    filterGroups: [lastModifiedDateFilter],
+    sorts: [{ propertyName: "lastmodifieddate", direction: "ASCENDING" }],
+    properties: [
+      "firstname",
+      "lastname",
+      "jobtitle",
+      "email",
+      "hubspotscore",
+      "hs_lead_status",
+      "hs_analytics_source",
+      "hs_latest_source",
+    ],
+    limit,
+    after: offsetObject.after,
+  };
+  return searchObject;
+}
+
+function processContactAction(companyAssociations, contact, lastPulledDate, q) {
+  const companyId = companyAssociations[contact.id];
+  const isCreated = new Date(contact.createdAt) > lastPulledDate;
+
+  const userProperties = {
+    company_id: companyId,
+    contact_name: (
+      (contact.properties.firstname || "") +
+      " " +
+      (contact.properties.lastname || "")
+    ).trim(),
+    contact_title: contact.properties.jobtitle,
+    contact_source: contact.properties.hs_analytics_source,
+    contact_status: contact.properties.hs_lead_status,
+    contact_score: parseInt(contact.properties.hubspotscore) || 0,
+  };
+
+  const actionTemplate = {
+    includeInAnalytics: 0,
+    identity: contact.properties.email,
+    userProperties: filterNullValuesFromObject(userProperties),
+  };
+
+  q.push({
+    actionName: isCreated ? "Contact Created" : "Contact Updated",
+    actionDate: new Date(isCreated ? contact.createdAt : contact.updatedAt),
+    ...actionTemplate,
+  });
+}
 
 module.exports = { processContacts };
